b22ab8728f676f9cf318389a89a67f26409e301d
Gabriel Richter <gabriel.richter@ambevtech.com.br>
2025-02-15 23:20:46 -0300

    feat: implementing keepass engine

--- vaultscan/engines/keepass.py
+++ vaultscan/engines/keepass.py
@@ -1,5 +1,6 @@
 from typing import Dict, List
 from dataclasses import dataclass
+from pykeepass import PyKeePass
 
 from vaultscan.engines.base import (
     FilterType,
@@ -37,6 +38,48 @@ class KeePassConfig(BaseVaultConfig):
 class KeepassSecretEngine(BaseVaultEngine):
     def __init__(self, vault: KeePassConfig):
         super().__init__(vault)
+        self.client = PyKeePass(filename = vault.path, password = vault.password)
 
     def find(self, filter: str, type: FilterType, is_value: bool = False) -> List[Secret]:
-        return list()
+        response = list()
+        entries = self._find(filter, type)
+        for entry in entries:
+            value = ''
+            if is_value:
+                value = entry.password
+            secret_name = KeepassSecretEngine.\
+                format_secret_name(
+                    group = str(entry.group),
+                    secret = str(entry.title)
+                )
+            response.append(
+                Secret(
+                    vault = self.vault.alias,
+                    name = secret_name,
+                    value = value
+                )
+            )
+        logger.debug(f'{len(response)} secrets found on KV {self.vault.alias} mathing the regex {filter}')
+        return response
+    
+    def _find(self, filter: str, type: FilterType):
+        if type == FilterType.BY_MATCH:
+            return self._find_by_match(filter)
+        return self._find_by_regex(filter)
+    
+    def _find_by_regex(self, filter: str):
+        # Case sensitive
+        return self.client.find_entries(title = f'{filter}.*', recursive = True, regex=  True)
+
+    def _find_by_match(self, filter: str):
+        # Case sensitive
+        return self.client.find_entries(title = filter, recursive = True)
+
+    @staticmethod
+    def format_secret_name(group: str, secret: str) -> str:
+        if not group:
+            return f'/{secret}'
+        # Initial value: Group: "Group1/Group2"
+        # Expected output: Group1/Group2
+        group = group.replace('Group: ', '').replace('"', '')
+        return f'{group}/{secret}'
