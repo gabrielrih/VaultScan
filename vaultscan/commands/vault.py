013babd649743cb9c411fe9c697efcdc631f7203
Gabriel Richter <gabriel.richter@ambevtech.com.br>
2025-02-17 17:52:40 -0300

    feat: enable/disable options for vaults

--- vaultscan/commands/vault.py
+++ vaultscan/commands/vault.py
@@ -1,6 +1,7 @@
 import click
 
 from vaultscan.repositories.vault.factory import VaultRepositoryFactory
+from vaultscan.engines.base import VaultStatus
 from vaultscan.engines.key_vault import KeyVaultConfig
 from vaultscan.engines.keepass import KeePassConfig
 from vaultscan.util.output.formatter import OutputFormat, OutputHandler
@@ -16,13 +17,13 @@ logger = LoggerFactory.get_logger(__name__)
 
 @click.group()
 def vault() -> None:
-    ''' Manage vaults '''
+    ''' Manage vaults on the configuration '''
     pass
 
 
 @vault.group
 def add() -> None:
-    ''' Add a new vault on the configuration '''
+    ''' Add a vault '''
     pass
 
 
@@ -45,8 +46,10 @@ def add() -> None:
               help = 'Azure Subscription ID where the vault is located')
 def kv(alias: str, vault_name: str, resource_group_name: str, subscription_id: str) -> None:
     ''' Azure Key Vault '''
+    logger.debug(f'Args: {str(locals())}')
     vault = KeyVaultConfig(
         alias = alias,
+        status = VaultStatus.ENABLED.value,
         subscription_id = subscription_id,
         resource_group_name = resource_group_name,
         vault_name = vault_name
@@ -69,12 +72,14 @@ def kv(alias: str, vault_name: str, resource_group_name: str, subscription_id: s
               help = 'Path of KDBX file')
 def keepass(alias: str, path: str) -> None:
     ''' KeePass database '''
+    logger.debug(f'Args: {str(locals())}')
     if repository.get(alias = alias):  # check in the first time to avoid prompting the password and them generating the error
         logger.warning(f'The alias "{alias}" is already registered! It must be unique.')
         return
     password = click.prompt(f'Password for {path}', hide_input = True, type = str)
     vault = KeePassConfig(
         alias = alias,
+        status = VaultStatus.ENABLED.value,
         path = path, 
         password = password
     )
@@ -91,7 +96,8 @@ def keepass(alias: str, path: str) -> None:
               required = True,
               help = 'Vault alias')
 def remove(alias: str) -> None:
-    ''' Remove a vault from the configuration'''
+    ''' Remove a vault '''
+    logger.debug(f'Args: {str(locals())}')
     removed = repository.remove(alias)
     if not removed:
         logger.warning(f'The alias "{alias}" was not found on the configuration!')
@@ -110,6 +116,7 @@ def remove(alias: str) -> None:
               help = 'Vault new alias')
 def rename(old_alias: str, new_alias: str) -> None:
     ''' Rename vault '''
+    logger.debug(f'Args: {str(locals())}')
     renamed = repository.rename(old_alias, new_alias)
     if not renamed:
         logger.warning(f'The alias "{old_alias}" was not found on the configuration!')
@@ -137,3 +144,33 @@ def list(output_format: str) -> None:
     vaults = repository.get_all()
     logger.info(f'{len(vaults)} vault(s) found!')
     OutputHandler(format).print(vaults)
+
+@vault.command()
+@click.option('--alias',
+              type = click.STRING,
+              required = True,
+              help = 'Vault alias')
+def disable(alias: str) -> None:
+    ''' Disable vault '''
+    logger.debug(f'Args: {str(locals())}')
+    status = VaultStatus.DISABLED
+    disabled = repository.change_status(alias = alias, status = status)
+    if not disabled:
+        logger.warning(f'The alias "{alias}" was not found on the configuration!')
+        return
+    logger.success(f'The status of alias "{alias}" has been changed to "{status.value}"!')
+
+@vault.command()
+@click.option('--alias',
+              type = click.STRING,
+              required = True,
+              help = 'Vault alias')
+def enable(alias: str) -> None:
+    ''' Enable vault '''
+    logger.debug(f'Args: {str(locals())}')
+    status = VaultStatus.ENABLED
+    disabled = repository.change_status(alias = alias, status = status)
+    if not disabled:
+        logger.warning(f'The alias "{alias}" was not found on the configuration!')
+        return
+    logger.success(f'The status of alias "{alias}" has been changed to "{status.value}"!')
