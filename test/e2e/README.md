caa9c40caa9247c7155a19e29c6f72658cbd62d2
Gabriel Richter <gabriel.richter@ambevtech.com.br>
2025-02-18 11:40:34 -0300

    doc: e2e documentation

---
+++ test/e2e/README.md
@@ -0,0 +1,10 @@
+# End to end tests
+
+The goal here is to test the actual tool running the commands the user would run. Example:
+
+```bash
+vaultscan version
+```
+
+So, before run it on the pipeline we need to install the package using pip, so you can run it using the real command.
+
---
+++ test/e2e/test_cli.py
@@ -0,0 +1,29 @@
+import subprocess
+
+from vaultscan.settings import PACKAGE_NAME
+
+
+class TestCliVersionCommand():
+    def test_version_command(self):
+        # Given
+        expected_output_message = f'{PACKAGE_NAME} version:'
+
+        # When
+        result = subprocess.run(
+            ['vaultscan', 'version'],  # Simulating the CLI execution
+            capture_output = True,  # Capture stdout and stderr
+            text = True  # Decode output as text (instead of bytes)
+        )
+
+        # Then
+        assert result.returncode == 0  # Ensure the command ran successfully
+        output_message = result.stdout.strip()
+        assert expected_output_message in output_message
+
+
+class TestCliVaultCommand():
+    pass
+
+
+class TestCliSecretCommand():
+    pass
