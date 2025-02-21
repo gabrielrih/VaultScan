90685434a704747afe2e33038d06a548f4fb4482
Gabriel Richter <gabriel.richter@ambevtech.com.br>
2025-02-21 12:01:16 -0300

    feat: getting secret values concurrently

--- vaultscan/core/engines/key_vault.py
+++ vaultscan/core/engines/key_vault.py
@@ -49,25 +49,83 @@ class KeyVaultSecretEngine(BaseVaultEngine):
         self.client = KeyVaultSecretClient(vault_name = vault.vault_name)
 
     def find(self, filter: str, type: FilterType, is_value: bool = False) -> List[Secret]:
-        filter = filter.lower()  # normalize the filter
-        response = list()
+        # Fetching all the secrets on the vault (without filter)
         secrets: List[str] = self.client.get_all_secrets()
-        for name in secrets:
-            name = name.lower()  # normalize the secret name
-            if not self.is_match([ name ], filter, type):
-                continue
-            value = ''
-            if is_value:
-                value = self.client.get_value(name)
-            response.append(
+
+        # Filterinf secret names based on your matching function.
+        # This reduces the set that may need the expensive "get_value" call.
+        filter = filter.lower()  # normalize the filter
+        matching_secrets = [ secret_name for secret_name in secrets if self.is_match([ secret_name.lower() ], filter, type) ]
+
+        # Return secrets without its value
+        response = list()
+        if not is_value:
+            response = [
                 Secret(
                     vault = self.vault.alias,
-                    name = name,
-                    value = value
+                    name = secret_name
                 )
-            )
+                for secret_name in matching_secrets
+            ]
+            logger.debug(f'{len(response)} secrets found on KV {self.vault.alias} mathing the regex {filter}')
+            return response
+
+        # Return secrets with its value
+        with concurrent.futures.ThreadPoolExecutor(max_workers = 3) as executor:
+            future_to_name = { executor.submit(self._fetch_secret_with_its_value, secret_name): secret_name for secret_name in matching_secrets }
+            for future in concurrent.futures.as_completed(future_to_name):
+                try:
+                    response.append(future.result())
+                except Exception as exc:
+                    logger.error("Error fetching secret %r: %s", future_to_name[future], exc)
         logger.debug(f'{len(response)} secrets found on KV {self.vault.alias} mathing the regex {filter}')
         return response
+    
+    def _fetch_secret_with_its_value(self, secret_name: str) -> Secret:
+        value = self.client.get_value(secret_name)
+        return Secret(
+            vault = self.vault.alias,
+            name = secret_name,
+            value = value
+        )
+
+        # for name in secrets:
+        #     name = name.lower()  # normalize the secret name
+        #     if not self.is_match([ name ], filter, type):
+        #         continue
+        #     value = ''
+        #     if is_value:
+        #         value = self.client.get_value(name)
+        #     response.append(
+        #         Secret(
+        #             vault = self.vault.alias,
+        #             name = name,
+        #             value = value
+        #         )
+        #     )
+        # logger.debug(f'{len(response)} secrets found on KV {self.vault.alias} mathing the regex {filter}')
+        # return response
+
+    # def find(self, filter: str, type: FilterType, is_value: bool = False) -> List[Secret]:
+    #     filter = filter.lower()  # normalize the filter
+    #     response = list()
+    #     secrets: List[str] = self.client.get_all_secrets()
+    #     for name in secrets:
+    #         name = name.lower()  # normalize the secret name
+    #         if not self.is_match([ name ], filter, type):
+    #             continue
+    #         value = ''
+    #         if is_value:
+    #             value = self.client.get_value(name)
+    #         response.append(
+    #             Secret(
+    #                 vault = self.vault.alias,
+    #                 name = name,
+    #                 value = value
+    #             )
+    #         )
+    #     logger.debug(f'{len(response)} secrets found on KV {self.vault.alias} mathing the regex {filter}')
+    #     return response
     
     # def find(self, filter: str, type: FilterType, is_value: bool = False) -> List[Secret]:
     #     filter = filter.lower()  # normalize the filter
