3b11efee5555edb4d44240110c78bd4a12932d09
Gabriel Richter <gabriel.richter@ambevtech.com.br>
2025-03-11 20:03:03 -0300

    fix: generating new version

--- vaultscan/main.py
+++ vaultscan/main.py
@@ -4,10 +4,6 @@ from vaultscan.commands.vault import vault
 from vaultscan.commands.find import find
 from vaultscan.commands.version import version
 
-
-__version__ = "0.1.0-rc.1"
-
-
 main.add_command(vault)
 main.add_command(config)
 main.add_command(find)
