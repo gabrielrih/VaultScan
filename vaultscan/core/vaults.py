e99561a7cfe7e4f2c7d7acb6f27065bbe081e334
Gabriel Richter <gabriel.richter@ambevtech.com.br>
2025-03-28 17:00:16 -0300

    test: adding unit test for vaults standalone method (inside the core)

--- vaultscan/core/vaults.py
+++ vaultscan/core/vaults.py
@@ -1,15 +1,14 @@
 from vaultscan.core.output.logger import LoggerFactory
 from vaultscan.repositories.vault.factory import VaultRepositoryFactory
 
-
 from typing import List, Dict
 
 
-vault_repository = VaultRepositoryFactory.create()
 logger = LoggerFactory.get_logger(__name__)
 
 
 def get_vaults(only_vault: str = '') -> List[Dict]:
+    vault_repository = VaultRepositoryFactory.create()
     if only_vault:
         vault: Dict = vault_repository.get(alias = only_vault)
         logger.debug(f'Vault "{only_vault}" content: {vault}')
